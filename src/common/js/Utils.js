bbb54a2c9991c2e21d0773be508ef88dff4595f3
renle <renle@zto.cn>
2019-11-09 17:51:33 +0800

    formatter utils

--- src/common/js/Utils.js
+++ src/common/js/Utils.js
@@ -1,7 +1,7 @@
 let FUNC_ERROR_TEXT = 'Expected a function'
 
 let nativeMax = Math.max // 原生最大值方法
-let nativeMin = Math.min// 原生最小值方法
+let nativeMin = Math.min // 原生最小值方法
 
 function debounce (func, wait, options) {
   let lastArgs, // 上次调用参数
@@ -22,15 +22,17 @@ function debounce (func, wait, options) {
     throw new TypeError(FUNC_ERROR_TEXT)
   }
   wait = +wait || 0
-  if (typeof (options) === 'object') { // 判断当前接收的的参数是否为object对象
+  if (typeof options === 'object') {
+    // 判断当前接收的的参数是否为object对象
     // 对配置的一些初始化
-    leading = !!options.leading
+    leading = !!options.leading // 指定在超时的前沿调用。
     maxing = 'maxWait' in options
-    maxWait = maxing ? nativeMax(options.maxWait * 1 || 0, wait) : maxWait
-    trailing = 'trailing' in options ? !!options.trailing : trailing
+    maxWait = maxing ? nativeMax(options.maxWait * 1 || 0, wait) : maxWait // 允许在调用之前延迟最大时间。
+    trailing = 'trailing' in options ? !!options.trailing : trailing // 指定在超时的后沿调用。
   }
 
-  function invokeFunc (time) { // 调用func，参数为当前时间
+  function invokeFunc (time) {
+    // 调用func，参数为当前时间
     const args = lastArgs // 调用参数
     const thisArg = lastThis // 调用的this
 
@@ -40,7 +42,8 @@ function debounce (func, wait, options) {
     return result
   }
 
-  function leadingEdge (time) { // 超时之前调用
+  function leadingEdge (time) {
+    // 超时之前调用
     // Reset any `maxWait` timer.
     lastInvokeTime = time // 设置上次调用时间为当前时间
     // 为 trailing edge 触发函数调用设定定时器
@@ -49,7 +52,8 @@ function debounce (func, wait, options) {
     return leading ? invokeFunc(time) : result // 如果leading为true，调用func,否则返回result
   }
 
-  function remainingWait (time) { // 设置还需要等待的时间
+  function remainingWait (time) {
+    // 设置还需要等待的时间
     const timeSinceLastCall = time - lastCallTime // 距离上次debounced函数被调用的时间 距离上次触发的时间
     const timeSinceLastInvoke = time - lastInvokeTime // 距离上次函数被执行的时间 距离上次调用func的时间
     const timeWaiting = wait - timeSinceLastCall // 用 wait 减去 timeSinceLastCall 计算出下一次trailing的位置 还需要等待的时间
@@ -68,13 +72,16 @@ function debounce (func, wait, options) {
     const timeSinceLastInvoke = time - lastInvokeTime // 距离上次调用func的时间
 
     // 几种满足条件的情况
-    return (lastCallTime === undefined || // 首次
-          (timeSinceLastCall >= wait) || // 距离上次被调用已经超过 wait
-          (timeSinceLastCall < 0) || // 系统时间倒退
-          (maxing && timeSinceLastInvoke >= maxWait)) // 超过最大等待时间
+    return (
+      lastCallTime === undefined || // 首次
+      timeSinceLastCall >= wait || // 距离上次被调用已经超过 wait
+      timeSinceLastCall < 0 || // 系统时间倒退
+      (maxing && timeSinceLastInvoke >= maxWait)
+    ) // 超过最大等待时间
   }
 
-  function timerExpired () { // 刷新timer
+  function timerExpired () {
+    // 刷新timer
     const time = Date.now()
     // 在 trailing edge 且时间符合条件时，调用 trailingEdge函数，否则重启定时器
     if (shouldInvoke(time)) {
@@ -84,7 +91,8 @@ function debounce (func, wait, options) {
     timerId = setTimeout(timerExpired, remainingWait(time)) // 不调用则重置timerId
   }
 
-  function trailingEdge (time) { // 超时之后调用
+  function trailingEdge (time) {
+    // 超时之后调用
     timerId = undefined
 
     // 有lastArgs才执行，意味着只有 func 已经被 debounced 过一次以后才会在 trailing edge 执行
@@ -97,7 +105,8 @@ function debounce (func, wait, options) {
     return result
   }
 
-  function cancel () { // 取消执行
+  function cancel () {
+    // 取消执行
     if (timerId !== undefined) {
       clearTimeout(timerId)
     }
@@ -105,7 +114,8 @@ function debounce (func, wait, options) {
     lastArgs = lastCallTime = lastThis = timerId = undefined
   }
 
-  function flush () { // 直接执行
+  function flush () {
+    // 直接执行
     return timerId === undefined ? result : trailingEdge(Date.now())
   }
 
@@ -120,7 +130,8 @@ function debounce (func, wait, options) {
     lastCallTime = time // 函数被调用的时间
 
     if (isInvoking) {
-      if (timerId === undefined) { // 无timerId的情况有两种：1.首次调用 2.trailingEdge执行过函数
+      if (timerId === undefined) {
+        // 无timerId的情况有两种：1.首次调用 2.trailingEdge执行过函数
         return leadingEdge(lastCallTime)
       }
       if (maxing) {
@@ -131,7 +142,8 @@ function debounce (func, wait, options) {
     }
     // 负责一种case：trailing 为 true 的情况下，在前一个 wait 的 trailingEdge 已经执行了函数；
     // 而这次函数被调用时 shouldInvoke 不满足条件，因此要设置定时器，在本次的 trailingEdge 保证函数被执行
-    if (timerId === undefined) { // 如果没有timer,设置定时器
+    if (timerId === undefined) {
+      // 如果没有timer,设置定时器
       timerId = setTimeout(timerExpired, wait)
     }
     return result
